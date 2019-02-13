ae1174ea9ecbf6a046a5df4fcc199c7504ae563a
jag378 <46541182+jag378@users.noreply.github.com>
2019-02-12 21:37:08 -0500

    Initial Date Entry

--- monthly_sales.py
+++ monthly_sales.py
@@ -21,6 +21,11 @@ csv_location = os.path.join(os.path.dirname(__file__), "data", csv_location)
 
 csv_data = pandas.read_csv(csv_location)
 
+#Month and Year Printout
+
+date_lookup = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
+
+user_date = input("Date:")
 
 #Calculating Sales Data
 
